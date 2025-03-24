4752c0554474a4452c65351b535882f8e4ed3151
monitor111 <lugovoy111wowa@gmail.com>
2025-03-24 14:32:10 +0200

    Your commit message

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <main class="blog">
     <div class="container">
-        <h1 class="edica-page-title" data-aos="fade-up">Блог</h1>
+        <h1 class="edica-page-title" data-aos="fade-up">Блог!</h1>
 
         <section class="featured-posts-section">
             <div class="row">
